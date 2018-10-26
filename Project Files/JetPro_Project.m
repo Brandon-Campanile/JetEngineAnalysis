1c78e5de33740d509819169c31db16ead5255073
lisakson <44508765+lisakson@users.noreply.github.com>
2018-10-26 16:10:09 -0400

    Update Project Files/JetPro_Project.m

---
+++ Project Files/JetPro_Project.m
@@ -0,0 +1,36 @@
+%% AE 4451 Jet & Rocket Propulsion Project
+%10/26/2018
+%Authors: Loren Isakson, Brandon Campanile, Matthew Yates
+
+%% Inputs
+T_a = 0;    %ambient temperature
+P_a = 0;    %ambient pressure
+P_f = 0;    %fuel storage pressure
+M = 0;      %flight mach number
+Pr_c = 0;   %compressor stagnation pressure ratio
+Pr_f = 0;   %fan stagnation pressure ratio
+f = 0;      %main burner fuel-air ratio
+f_ab = 0;   %afterburner fuel-air ratio
+Beta = 0;   %bypass ratio
+b = 0;      %bleed ratio
+
+%% Function Library
+%Ambient conditions provided T_a and P_a
+
+function [T_o2, P_o2] = diffuser(T_o1, P_o1)
+
+end
+
+function [T_o2, P_o2] = compressor(T_o1, P_o1)
+
+end
+
+function [T_o3, P_o3] = 
+
+
+
+
+
+
+
+%
