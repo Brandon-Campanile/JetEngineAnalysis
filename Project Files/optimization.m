50f1609d3b9b8eb807999070d40e7405749df040
Brandon-Campanile <41024248+Brandon-Campanile@users.noreply.github.com>
2018-11-17 10:57:37 -0500

    Update optimization.m

--- Project Files/optimization.m
+++ Project Files/optimization.m
@@ -11,24 +11,24 @@ minVar = cell(1,2);
 for ab=0:1
     if eType % tubofan
         if ab % w/ afterburner
-            % [bypass ratio, fan pressure ratio, fuel-air ratio, afterburner fuel-air ratio, bleed ratio]
-            lb = [0.001, 1.1, .001, .0005, 0]; % lower bound
-            ub = [10, 1.5, .1, .1, .12];        % upper bound
-            x0 = [5, 1.25, .05, .03, .05];
+            % [bypass ratio, fan pressure ratio, compressor pressure ratio, fuel-air ratio, afterburner fuel-air ratio, bleed ratio]
+            lb = [0.001, 1.1, 1, .001, .0005, 0]; % lower bound
+            ub = [10, 1.5, 54.545455, .1, .1, .12];        % upper bound
+            x0 = [5, 1.25, 30, .05, .03, .05];
         else
-            lb = [.001, 1.1, .001, 0, 0];
-            ub =[10, 1.5, .1, 0, .12];
-            x0 = [5, 1.25, .05, 0, .05];
+            lb = [.001, 1.1, 1, .001, 0, 0];
+            ub =[10, 1.5, 54.545455, .1, 0, .12];
+            x0 = [5, 1.25, 30, .05, 0, .05];
         end
     else % turbojet
         if ab % w/ afterburner
-            lb = [0, 1.1, .001, .0005, 0];
-            ub = [0, 1.5, .1, .1, .12];
-            x0 = [0, 1.25, .05, .03, .05];
+            lb = [0, 1.1, 1, .001, .0005, 0];
+            ub = [0, 1.5, 54.545455, .1, .1, .12];
+            x0 = [0, 1.25, 30, .05, .03, .05];
         else
-            lb = [0, 1.1, .001, 0, 0];
-            ub =[0, 1.5, .1, 0, .12];
-            x0 = [0, 1.25, .05, 0, .05];
+            lb = [0, 1.1, 1, .001, 0, 0];
+            ub =[0, 1.5, 54.545455, .1, 0, .12];
+            x0 = [0, 1.25, 30, .05, 0, .05];
         end
     end
     
@@ -52,10 +52,10 @@ end
 function TSFC2 = singleOut(x, T, eType, Nmix, Ta, Pa, Pf, M, Prb, Prab, Prnm, Tomax, Tmax_ab, MW, eff, y, HVf)
 beta = x(1);
 Prf = x(2);
-Prc = 60/x(2);
-f = x(3);
-fab = x(4);
-b = x(5);
+Prc = x(3);
+f = x(4);
+fab = x(5);
+b = x(6);
 output = JetPro_Project(T, eType, Nmix, Ta, Pa, Pf, M, Prf, Prc, Prb, Prab, Prnm, beta, b, f, fab, Tomax, Tmax_ab, MW, eff, y, HVf);
 TSFC2 = output(2);
 end
@@ -65,10 +65,10 @@ R=8314;
 CB=700;
 beta = x(1);
 Prf = x(2);
-Prc = 60/x(2);
-f = x(3);
-fab = x(4);
-b = x(5);
+Prc = x(3);
+f = x(4);
+fab = x(5);
+b = x(6);
 
 out2 = JetPro_Project(T, eType, Nmix, Ta, Pa, Pf, M, Prf, Prc, Prb, Prab, Prnm, beta, b, f, fab, Tomax, Tmax_ab, MW, eff, y, HVf);
 
@@ -76,15 +76,16 @@ Cp1 = y(4)*(R/MW(4))/(y(4)-1);
 Cp2 = y(8)*(R/MW(8))/(y(8)-1);
 fmax = (1-b)*(1-out2(3)/Tomax)/(eff(4)*HVf/Cp1/Tomax - 1);
 
-g(1) = x(3) - fmax; % f <= fmax
-g(2) = (out2(3)+f*HVf/Cp1)/(1+f-b) - Tomax - CB*(b/.12)^.5; % Tb <= Tmax
-g(3) = ST*1000-out2(1); % ST >= STdesired
+g(1) = Prc - 60/Prf; % prc<=60/prf
+g(2) = f - fmax; % f <= fmax
+g(3) = (out2(3)+f*HVf/Cp1)/(1+f-b) - Tomax - CB*(b/.12)^.5; % Tb <= Tmax
+g(4) = ST*1000-out2(1); % ST >= STdesired
 if fab>0
-    g(4) = (out2(4) + (f+fab)*HVf/Cp2)/(1+f+fab) - Tmax_ab; % Tab <= Tmaxab
-    g(5) = x(4) - (1+fmax)*(Tmax_ab/out2(4) - 1)/((eff(7)*HVf/Cp2 - Tmax_ab)/out2(4)); % fab <= fmaxab
+    g(5) = (out2(4) + (f+fab)*HVf/Cp2)/(1+f+fab) - Tmax_ab; % Tab <= Tmaxab
+    g(6) = fab - (1+fmax)*(Tmax_ab/out2(4) - 1)/((eff(7)*HVf/Cp2 - Tmax_ab)/out2(4)); % fab <= fmaxab
 else
-    g(4)=0;
     g(5)=0;
+    g(6)=0;
 end
 ceq = [];
 end
