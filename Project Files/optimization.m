e716f75afe79d802e31957d78aa263f7eff2c704
Brandon-Campanile <41024248+Brandon-Campanile@users.noreply.github.com>
2018-11-18 01:59:20 -0500

    Update optimization.m
    removed 'patternsearch' and replaced back with fmincon, but with the algorithm 'sqp'.

--- Project Files/optimization.m
+++ Project Files/optimization.m
@@ -13,11 +13,11 @@ for ab=0:1
         if ab % w/ afterburner
             % [bypass ratio, fan pressure ratio, compressor pressure ratio, fuel-air ratio, afterburner fuel-air ratio, bleed ratio]
             lb = [0.001, 1.1, 10, .001, .0005, 0]; % lower bound
-            ub = [12, 1.5, 54.545455, .1, .1, .12];        % upper bound
+            ub = [10, 1.5, 54.545455, .1, .1, .12];        % upper bound
             x0 = [5, 1.25, 40, .05, .03, .05];
         else
             lb = [.001, 1.1, 10, .001, 0, 0];
-            ub =[12, 1.5, 54.545455, .1, 0, .12];
+            ub =[10, 1.5, 54.545455, .1, 0, .12];
             x0 = [5, 1.25, 40, .05, 0, .05];
         end
     else % turbojet
@@ -36,7 +36,9 @@ for ab=0:1
     
     nlc = @(x)nonlcon(x, T, ST, eType, Nmix, Ta, Pa, Pf, M, Prb, Prab, Prnm, Tomax, Tmax_ab, MW, eff, y, HVf);
     
-    b_min = patternsearch(func, x0, A, c, Aeq, beq, lb, ub, nlc);
+    options=optimoptions('fmincon','Display','iter','Algorithm','sqp');
+    
+    b_min = fmincon(func, x0, A, c, Aeq, beq, lb, ub, nlc, options);
     
     minVar{ab+1} = b_min;
     
@@ -69,12 +71,14 @@ Prc = x(3);
 f = x(4);
 fab = x(5);
 b = x(6);
+bmax=.12;
 
 out2 = JetPro_Project(T, eType, Nmix, Ta, Pa, Pf, M, Prf, Prc, Prb, Prab, Prnm, beta, b, f, fab, Tomax, Tmax_ab, MW, eff, y, HVf);
 
 Cp1 = y(4)*(R/MW(4))/(y(4)-1);
 Cp2 = y(8)*(R/MW(8))/(y(8)-1);
-fmax = (1-b)*(1-out2(3)/Tomax)/(eff(4)*HVf/Cp1/Tomax - 1);
+Tmax = Tomax + CB*(b/bmax)^0.5;
+fmax = (1-b)*(1-out2(3)/Tmax)/(eff(4)*HVf/Cp1/Tmax - 1);
 
 g(1) = Prc - 60/Prf; % prc<=60/prf
 g(2) = f - fmax; % f <= fmax
